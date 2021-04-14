1be5e8ac38596af7203b61dec66e1da2733633d4
kashi326 <kashifahmad0332@gmail.com>
2021-04-14 21:16:59 +0500

    homepage changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -278,6 +278,7 @@
                                         {{ __('Logout') }}
                                     </a>
 
+
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
